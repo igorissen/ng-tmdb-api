e0a777c99dfb0086ac0a8a393ba0adeea00d53cb
Ismael Gorissen <ismael.gorissen@gmail.com>
2019-08-08 22:13:45 +0200

    🎉 Create library

---
+++ projects/tmdb-api/src/lib/tmdb-api.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { TmdbApiComponent } from './tmdb-api.component';
+
+
+
+@NgModule({
+  declarations: [TmdbApiComponent],
+  imports: [
+  ],
+  exports: [TmdbApiComponent]
+})
+export class TmdbApiModule { }
---
+++ projects/tmdb-api/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of tmdb-api
+ */
+
+export * from './lib/tmdb-api.service';
+export * from './lib/tmdb-api.component';
+export * from './lib/tmdb-api.module';
